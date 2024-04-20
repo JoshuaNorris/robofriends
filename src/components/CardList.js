add835c7c6ea5a903c1cb9a037d8e541c80290fe
Joshua Norris <joshua.norris52@gmail.com>
2024-04-20 16:48:58 -0500

    cleaning structure

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Card from './card.js'; // Assuming the filename is 'Card.js' with a capital 'C'
-import Robots from './robots.js'; // Assuming the filename is 'Robots.js' with a capital 'R'
+import Card from './card.js'; // Assuming the filename is 'Card.js' with a capital 'C'// Assuming the filename is 'Robots.js' with a capital 'R'
 
 const CardList = ({ robots }) => {
     return (
